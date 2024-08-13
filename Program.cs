7b1eee535b9a6392de315ec0ef12dc7686396b01
evandro-ribeiro <evandrormf.dev@gmail.com>
2024-08-12 21:04:59 -0300

    Adicionando projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Console.WriteLine("Digite o número de hóspedes:");
+int numeroHospedes = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numeroHospedes; i++)
+{
+    hospedes.Add(new Pessoa(nome: $"Hóspede {i}"));
+}
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
+
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: R${reserva.CalcularValorDiaria():F2}");
---
+++ README.md
@@ -0,0 +1,7 @@
+# Sistema de Hospedagem - C#
+
+Construção de um sistema de hospedagem utilizando a linguagem C#, que será usado para realizar uma reserva em um hotel. Será utilizado a classe Pessoa, que representa o hóspede, a classe Suíte, e a classe Reserva, que fará um relacionamento entre ambos.
+
+O programa calcula os valores dos métodos da classe Reserva, com base na quantidade de hóspedes e o valor da diária, concedendo um desconto de 10% para caso a reserva seja para um período maior que 10 dias.
+
+![Diagrama de classe estacionamento](diagrama_classe_hotel.png)
